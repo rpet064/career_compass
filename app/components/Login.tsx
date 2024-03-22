0035edbcb78777e2deaceb6cdcf0c3550a1ff894
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-23 00:02:25 +1300

    Begin implementing login page styling
    Implement css for login page and colours across the application
    Begin implementation of html elements

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,5 +1,31 @@
+import styles from "../styles/login.module.css";
+import colours from "../styles/colour.module.css";
+
 export default function Login() {
  return (
-    <div>This is the Login</div>
+   <div className={`${styles.loginContainer} ${colours.grayBorder}`}>
+       {/* Header */}
+      <div className={`${styles.header} ${colours.orangeBackground}`}>
+         <div>Icon goes here</div>
+         <span>Sign In  |  Career Compass</span>
+      </div>
+
+      {/* Body */}
+      <div className={styles.body}>
+         <div>
+            <span className={styles.inputLabel}>Login</span>
+            <input></input>
+         </div>
+         <div>
+            <span className={styles.inputLabel}>Password:</span>
+            <input></input>
+         </div>
+      </div>
+
+       {/* Footer */}
+       <div className={styles.footer}>
+       <button className={styles.loginButton}>Login</button>
+      </div>
+   </div>
  );
 }
