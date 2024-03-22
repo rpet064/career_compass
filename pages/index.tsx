fd36a372994e262a502fcf3dfabb8eb4cda755dc
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-22 22:58:29 +1300

    Remove Tailwind CSS from project
    Remove Tailwind CSS from project

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Main() {
  return (
     <>
       {isUserLoggedIn ? (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main>
           <Navbar />
           <JobApplication />
           <UserProfile />
