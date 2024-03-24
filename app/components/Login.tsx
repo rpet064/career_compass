10cfd2f4c75288e40df2ec73e76596c17d1fd7b8
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-24 22:26:11 +1300

    Complete styling of login page
    Complete styling of login page
    Use FaCompass instead of FiCompass
    Begin to implement dynamic login container height (depending on if error message is displayed)

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,31 +1,48 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/login.module.css";
 import colours from "../styles/colour.module.css";
-import { FiCompass } from "react-icons/fi";
+import { FaCompass } from "react-icons/fa";
 
 export default function Login() {
+
+   const [displayErrorMessage, setDisplayErrorMessage] = useState(null);
+   const [loginContainerHeight, settingsContainerHeight] = useState("30%");
+   
+   // Increase container width if displaying error message
+   useEffect(() => {
+      if (displayErrorMessage !== null) {
+         settingsContainerHeight("32.5%");
+      } else {
+         settingsContainerHeight("30%");
+      }
+   }, [displayErrorMessage]);
+
  return (
-   <div className={`${styles.loginContainer} ${colours.grayBorder}`}>
+   <div style={{ height: loginContainerHeight }}
+   className={`${styles.loginContainer} ${colours.grayBoxShadow}`}>
        {/* Header */}
       <div className={`${styles.header} ${colours.orangeBackground}`}>
-         <FiCompass />
+         <FaCompass className={colours.whiteFont} />
          <span>Sign In</span><span>|</span> <span>Career Compass</span>
       </div>
 
       {/* Body */}
       <div className={styles.body}>
-         <div>
-            <span className={styles.inputLabel}>Login</span>
+         <div className={styles.inputContainer}>
+            <span className={`${styles.inputLabel} ${colours.lightGrayFont}`}>Username</span>
             <input></input>
+            <span className={styles.authenticationLabel}>This field is required</span>
          </div>
-         <div>
-            <span className={styles.inputLabel}>Password:</span>
+         <div className={styles.inputContainer}>
+            <span className={`${styles.inputLabel} ${colours.lightGrayFont}`}>Password</span>
             <input></input>
+            <span className={styles.authenticationLabel}>This field is required</span>
          </div>
       </div>
 
        {/* Footer */}
        <div className={styles.footer}>
-       <button className={styles.loginButton}>Login</button>
+       <button className={`${styles.loginButton}`}>Login</button>
       </div>
    </div>
  );
