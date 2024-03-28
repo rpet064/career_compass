0dcf480f95d68ede76457cb106f527ab90a85d7a
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-28 23:05:05 +1300

    Implement navbar elements and styling
    Implement navbar elements and styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,28 +1,27 @@
 import { FiBriefcase, FiUser, FiRefreshCcw, FiLogIn, FiLogOut, FiCompass } from "react-icons/fi";
 import { useState } from "react";
+import styles from '../styles/navbar.module.css';
+import colour from '../styles/colour.module.css';
 
 export default function Navbar() {
 
    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-   const [displayLogo, setDsplayLogo] = useState(false);
+   const [displayLogo, setDisplayLogo] = useState(false);
 
-   const toggleDisplayLogo = () => {
-      setDsplayLogo(!displayLogo);
-   }
+   return (
+      <div className={`${styles.navbar} ${colour.lightGrayBoxShadow} ${colour.orangeBackground}`}>
+         <div>
+            <button onClick={() => setDisplayLogo(!displayLogo)}>{displayLogo ? <FiCompass /> : 
+            <span className={styles.logoText}>Career Compass</span>}</button>
+         </div>
 
- return (
-    <div>
-      <div>
-         {displayLogo? <FiCompass /> : <span>Career Compass</span>}
+         {/* navigation */}
+         <div>
+            {isUserLoggedIn ? <FiLogIn /> : <FiLogOut />}
+            <FiBriefcase />
+            <FiUser />
+            <FiRefreshCcw />
+         </div>
       </div>
-
-      {/* navigation */}
-      <div>
-         {isUserLoggedIn? <FiLogIn /> : <FiLogOut /> }
-         <FiBriefcase />
-         <FiUser />
-         <FiRefreshCcw />
-      </div>
-    </div>
- );
+   );
 }
