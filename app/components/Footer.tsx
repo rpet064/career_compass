7e8cf04379ab4371c45a35f01980092342dc8a70
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-28 23:04:54 +1300

    Implement footer elements and styling
    Implement footer elements and styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,19 @@
 import { FiGithub, FiLinkedin, FiMail } from 'react-icons/fi';
+import styles from '../styles/footer.module.css';
+import colour from '../styles/colour.module.css';
 
 export default function Navbar() {
- return (
-   <div>
-   <a href="https://github.com/rpet064">
-       <FiGithub title="Github" />
-   </a>
-   <a href="https://www.linkedin.com/in/robert-pether-ba9968113">
-       <FiLinkedin title="Linkedin" />
-   </a>
-   <a href="mailto:rpether@hotmail.co.nz">
-       <FiMail title="Email" />
-   </a>
-</div>
- );
+    return (
+        <div className={`${styles.footer} ${colour.lightGrayBoxShadow} ${colour.orangeBackground}`}>
+            <a href="https://github.com/rpet064">
+                <FiGithub title="Github" />
+            </a>
+            <a href="https://www.linkedin.com/in/robert-pether-ba9968113">
+                <FiLinkedin title="Linkedin" />
+            </a>
+            <a href="mailto:rpether@hotmail.co.nz">
+                <FiMail title="Email" />
+            </a>
+        </div>
+    );
 }
