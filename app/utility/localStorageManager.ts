8b5264d351078250e807916c2d6f1633f0265983
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-04-25 21:38:33 +1200

    Implement managing tokens in client local storage
    Implement getTokenFromLocalStorage
    Implement clearAuthTokenFromLocalStorage
    Implement createNewTokenInLocalStorage

--- app/utility/localStorageManager.ts
+++ app/utility/localStorageManager.ts
@@ -1,11 +1,18 @@
-export const getAuthToken = (): string | null => {
-    return null;
+// TODO: Does this need try/catch
+// TODO: IF not explore how to check if token created sucessfully or not? 
+// Could I use getTokenFromLocalStorage to check clearAuthTokenFromLocalStorage and createNewTokenInLocalStorage
+
+export const getTokenFromLocalStorage = (): string | null => {
+    const token = localStorage.getItem('authToken');
+    return token ? token : null;
 };
 
-export const clearAuthToken = (): boolean => {
+export const clearAuthTokenFromLocalStorage = (): boolean => {
+    localStorage.removeItem('authToken');
     return true;
 };
 
-export const saveNewToken = (): boolean => {
+export const createNewTokenInLocalStorage = (token: string): boolean => {
+    localStorage.setItem('authToken', token);
     return true;
 };
