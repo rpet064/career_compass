3d7c869ff04a54363a2451e0592bcec03b45f5f8
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-06-02 22:19:54 +1200

    Adjust job-applications/get.tsx to accept userid as a parameter
    Adjust job-applications/get.tsx to accept userid as a parameter

--- pages/api/job-applications/get.tsx
+++ pages/api/job-applications/get.tsx
@@ -4,31 +4,30 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async function logOutHandler (req: NextApiRequest, res: NextApiResponse){
-        if (req.method !== 'GET') {
-            return res.status(405).json({ message: 'Method Not Allowed' });
-        }
-
-        const { userId } = req.body;
-
-        if (!userId) {
-            return res.status(400).json({ message: 'User id is required' });
-        }
-        
-        let userIdAsInt
-        try{
-            userIdAsInt = parseInt(userId);
-        } catch {
-            return res.status(400).json({ message: 'User id must be a number' });
-        }
-
-        let jobApplicationsList = await getAllJobApplicationsFromDatabase(userIdAsInt)
-    
-        return res.status(200).json({ 
-            message: `Job applications for user ${userId}`,
-            jobApplicationsList: jobApplicationsList
-        });
-    }  
+export default async function getUserJobApplications(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method!== 'GET') {
+      return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  
+    const { userid } = req.query;
+    if (!userid || typeof userid!== 'string') {
+      return res.status(400).json({ message: 'User id is required' });
+    }
+  
+    let userIdAsInt;
+    try {
+      userIdAsInt = parseInt(userid, 10);
+    } catch {
+      return res.status(400).json({ message: 'User id must be a number' });
+    }
+  
+    let jobApplicationsList = await getAllJobApplicationsFromDatabase(userIdAsInt);
+  
+    return res.status(200).json({
+      message: `Job applications for user ${userid}`,
+      jobApplicationsList: jobApplicationsList,
+    });
+  }
 
 async function getAllJobApplicationsFromDatabase(userid: number): Promise<jobapplications[] | null> {
     try{
