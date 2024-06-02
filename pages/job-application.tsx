ebf20fe3d1a846023ea038ab7d078035dbf7c8dd
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-06-02 22:23:40 +1200

    Implement displaying and fetching of jobApplication data in JobApplication
    Enable JobApplication to fetch data
    Implement JobApplicationDataTable component
    Display LoadingSpinner when loading api data
    Display <p>No job applications found.</p> if user doesn't have any job applications

--- pages/job-application.tsx
+++ pages/job-application.tsx
@@ -1,16 +1,81 @@
-import Navbar from "../app/components/Navbar";
-import Footer from "../app/components/Footer";
+import Navbar from "../app/customComponents/Navbar";
+import Footer from "../app/customComponents/Footer";
 import { checkAuth } from '../app/utility/checkAuth';
 import UserProps from "@/app/interfaces/userProps";
 import { NextPageContext } from 'next';
+import JobApplicationDataTable from "@/app/primeReactComponents/JobApplicationDataTable";
+import LoadingSpinner from "../app/customComponents/LoadingSpinner";
+import React, { useState, useEffect } from 'react';
 
   export default function JobApplication({ userid, username }: UserProps) {
+    const [userId, setUserId] = useState(1);
+    const [jobApplicationData, setJobApplicationData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    if (userid === -1){
+      userid = 1;
+      setUserId(userid);
+    }
+
+    const getJobApplication = async (userId: number) => {
+      try {
+        const url = new URL('http://localhost:3000/api/job-applications/get');
+        url.searchParams.append('userid', userId.toString());
+    
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded', 
+          },
+        });
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching job application:', error);
+      }
+    };
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await getJobApplication(userId);
+          if(data.jobApplicationsList.length < 1) {
+            return;
+          }
+
+          console.log(data.jobApplicationsList)
+
+          setJobApplicationData(data.jobApplicationsList);
+          setIsLoading(false); 
+        } catch (error) {
+          console.error('Error fetching job application:', error);
+        }
+      };
+  
+      fetchData();
+    }, [userId]);
+  
+
     return (
       <main>
       <Navbar userid={userid} />
       <section>
         <h1>This is the job application</h1>
-        <div>Welcome, {username}!</div>
+        <div>Welcome, {userId}!</div>
+        {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        jobApplicationData ? (
+          <JobApplicationDataTable jobApplicationData={jobApplicationData} />
+        ) : (
+          <p>No job applications found.</p>
+        )
+      )}
       </section>
       <Footer />
       </main>
