89d854941a8426026047e9333177e28f78abcb5a
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-06-08 00:10:11 +1200

    Navbar component adjustment
    Footer & header use relative path for css imports (compiler error)
    Move handleLogout to apiProxy

--- app/customComponents/Navbar.tsx
+++ app/customComponents/Navbar.tsx
@@ -1,44 +1,13 @@
 import { FiBriefcase, FiUser, FiRefreshCcw, FiLogOut, FiCompass, FiHome } from "react-icons/fi";
 import { FC } from "react";
-import styles from '@app/styles/navbar.module.css';
-import colour from '@app/styles/colour.module.css';
+import styles from '../styles/navbar.module.css';
+import colour from '../styles/colour.module.css';
 import { useNavigation } from '../utility/navigation';
-import { clearAuthTokenFromLocalStorage } from '../utility/localStorageManager';
 import NavbarProps from "../interfaces/navbarProps";
+import { HandleLogout } from '../proxyApi/authentication/handleLogout';
 
 const Navbar : FC<NavbarProps> = ({ userid }) => {
    const navigate = useNavigation();
-   const handleLogout = async () => {
-
-      let logoutData = {
-         userid: userid,
-      }
-
-      try {
-         const response = await fetch("/api/authentication/logout", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(logoutData),
-        });
-
-         const data = await response.json();
-
-          if(!data){
-            throw Error("Error logging out. Please try again.")
-          }
-          // Clear token from client local storage
-          let isTokenClearedFromLocalStorge = clearAuthTokenFromLocalStorage();
-          if(!isTokenClearedFromLocalStorge){
-            throw new Error("Token could not be cleared from local storage")
-          }
-
-          navigate("/login");
-      } catch (error) {
-         console.log(error);
-      }
-   }
 
    const navigateUser = (nextPage: string) => {
 
@@ -62,7 +31,7 @@ const Navbar : FC<NavbarProps> = ({ userid }) => {
             <FiBriefcase onClick={() => navigateUser("job-application")}/>
             <FiUser onClick={() => navigateUser("user-profile")}/>
             <FiRefreshCcw onClick={() => location.reload()}/>
-            <FiLogOut onClick={() => handleLogout}/>
+            <FiLogOut onClick={() => HandleLogout}/>
          </div>
       </div>
    );
---
+++ app/proxyApi/authentication/handleLogout.ts
@@ -0,0 +1,36 @@
+import { clearAuthTokenFromLocalStorage } from '../../utility/localStorageManager';
+import { useNavigation } from '../../utility/navigation';
+
+export const HandleLogout = async (userid: number) => {
+
+    const navigate = useNavigation();
+
+    let logoutData = {
+       userid: userid,
+    }
+
+    try {
+       const response = await fetch("/api/authentication/logout", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(logoutData),
+      });
+
+       const data = await response.json();
+
+        if(!data){
+          throw Error("Error logging out. Please try again.")
+        }
+        // Clear token from client local storage
+        let isTokenClearedFromLocalStorge = clearAuthTokenFromLocalStorage();
+        if(!isTokenClearedFromLocalStorge){
+          throw new Error("Token could not be cleared from local storage")
+        }
+
+        navigate("/login");
+    } catch (error) {
+       console.log(error);
+    }
+ }
