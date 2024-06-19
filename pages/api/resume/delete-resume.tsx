fc0e9d06d6bf0fdbc4e80a393fba4d5021867283
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-06-19 23:17:38 +1200

    Implement deleting resume and getting resumes api
    Implement deleting resume api
    Implement getting all resumes

---
+++ pages/api/resume/delete-resume.tsx
@@ -0,0 +1,64 @@
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getUserDetailsFromDatabase } from "../user/get-user";
+
+const prisma = new PrismaClient();
+
+export default async function deleteResumeHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'PUT') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+
+    const { userId, resumeId } = req.query;
+
+    if (!userId || typeof userId !== 'string') {
+        return res.status(400).json({ message: 'User id is required' });
+    }
+
+    if (!resumeId || typeof resumeId !== 'string') {
+        return res.status(400).json({ message: 'resume id is required' });
+    }
+
+    let userIdAsInt, resumeIdAsInt;
+    try {
+        userIdAsInt = parseInt(userId, 10);
+    } catch {
+        return res.status(400).json({ message: 'User id must be a number' });
+    }
+
+    try {
+        resumeIdAsInt = parseInt(resumeId, 10);
+    } catch {
+        return res.status(400).json({ message: 'resume id must be a number' });
+    }
+
+    let userDetails = await getUserDetailsFromDatabase(userIdAsInt)
+    if (!userDetails)
+      return res.status(400).json({ message: 'Invalid User'});
+
+    // TODO: Update resume schema to include userid (so users can't delete other user's resumes)
+
+    let isResumeDeleted = await deleteResumeFromDatabase(resumeIdAsInt);
+
+    return res.status(200).json({
+        message: `Resume ${resumeId} is deleted`,
+        isResumeDeleted: isResumeDeleted,
+    });
+}
+
+async function deleteResumeFromDatabase(resumeId: number): Promise<boolean> {
+    try {
+        await prisma.resume.update({
+            where: {
+                resumeid: resumeId,
+            },
+            data: {
+                whendeleted: new Date(),
+            },
+
+        });
+        return true;
+    } catch (error: any) {
+        return false;
+    }
+}
---
+++ pages/api/resume/get-resumes.tsx
@@ -0,0 +1,49 @@
+import { resume, PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getUserDetailsFromDatabase } from "../user/get-user";
+
+const prisma = new PrismaClient();
+
+export default async function getResumesHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method!== 'GET') {
+      return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  
+    const { userId } = req.query;
+    
+    if (!userId || typeof userId!== 'string') {
+      return res.status(400).json({ message: 'Authenticated user is is required' });
+    }
+
+    let userIdAsInt;
+    try {
+      userIdAsInt = parseInt(userId, 10);
+    } catch {
+      return res.status(400).json({ message: 'User id must be a number'});
+    }
+
+    let userDetails = await getUserDetailsFromDatabase(userIdAsInt)
+    if (!userDetails)
+      return res.status(400).json({ message: 'Invalid User'});
+
+    let resumes = await getResumesFromDatabase()
+  
+    return res.status(200).json({
+      message: `Resumes for user ${userId}`,
+      resumeList: resumes,
+    });
+  }
+
+// TODO: Check if resumes belong to user
+async function getResumesFromDatabase(): Promise<resume[] | null> {
+    try{
+        let resumes = await prisma.resume.findMany({
+            where: {
+                whendeleted: null,
+            }
+        });
+        return resumes
+    } catch (error){
+        return null;
+    }
+}
