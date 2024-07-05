22caae7b7d58a8ca8765b4b3de8172903ac082b7
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-07-05 23:54:28 +1200

    Implement fetching of data in individual details pages
    Implement fetching of data in user, resume & job application pages
    Enable tracking if record is new
    Safely parse parameter id into id to fetch data

--- pages/manage-resume/[id].tsx
+++ pages/manage-resume/[id].tsx
@@ -1,13 +1,53 @@
+import { getResumeDetails } from '@/proxyApi/resume/getResumeDetails';
+import { resume } from '@prisma/client';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function ManageResume(){
+
     const router = useRouter();
     const { id } = router.query;
-    if (!id) {
-        return <div>Loading...</div>;
+
+    let resumeId: number;
+    try{
+        resumeId = id ? parseInt(id[0]) : -1;
+    } catch (e) {
+        throw new Error("Could not set resume id")
     }
 
+    let isNewResume = false;
+    if(resumeId > 0)
+        isNewResume = true;
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [resumeDetails, setResumeDetails] = useState<resume>({
+      resumeid: -1,
+      resumename: '',
+      resumedescription: '',
+      resumeurl: '',
+      whencreated: new Date,
+      whendeleted: null
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const data = await getResumeDetails(resumeId);
+            if (data.resumeDetails.length < 1) {
+              return;
+            }
+    
+            setResumeDetails(data.resumeDetails);
+            setIsLoading(false);
+          } catch (error) {
+            console.error('Error fetching resume details:', error);
+          }
+        };
+    
+        fetchData();
+      }, [resumeId]);
+
     return (
-        <p>ID: {id}</p>
+        <p>ID: {resumeId}</p>
     );
 }
