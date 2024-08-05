9b58935df4dc66e835cd75f99a1cfbdec6b32e07
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-08-05 23:05:59 +1200

    Implement progressOptions and  sentimentOptions
    Implement progressOptions and  sentimentOptions

---
+++ app/interfaces/progressOptions.ts
@@ -0,0 +1,9 @@
+export const progressOptions = [
+    "Not yet submitted",
+    "In progress",
+    "Submitted",
+    "Interview",
+    "References",
+    "Successful",
+    "Unsuccessful"
+];
---
+++ app/interfaces/sentimentOptions.ts
@@ -0,0 +1,7 @@
+export const sentimentOptions = [
+    "FiFrown",
+    "FiThumbsDown",
+    "FiMeh",
+    "FiThumbsUp",
+    "FiSmile",
+];
