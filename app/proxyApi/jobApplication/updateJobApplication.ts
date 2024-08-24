1add70e1a241638859858d3c25074ede56e03036
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-08-24 23:30:04 +1200

    Implement updateJobApplication proxy api
    Implement updateJobApplication proxy api

--- app/proxyApi/jobApplication/updateJobApplication.ts
+++ app/proxyApi/jobApplication/updateJobApplication.ts
@@ -1,2 +1,29 @@
-export const updateJobApplication = async () => {
+import { jobapplications } from '@prisma/client';
+
+export const updateJobApplication = async (jobapplicationList: jobapplications[], userId: number) => {
+    try {
+        const url = new URL('http://localhost:3000/api/job-applications/update-job-applications');
+
+        const payload = {
+            jobapplicationList: jobapplicationList,
+            userid: userId.toString(),
+        };
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error updating job applications:', error);
+    }
 };
